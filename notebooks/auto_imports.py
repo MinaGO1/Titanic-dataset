27ba9a7cab373d0599d101d2b91f54232adb7081
Mina <salipmina123@gmail.com>
2025-06-11 17:05:29 +0300

    Stracture_project

---
+++ notebooks/auto_imports.py
@@ -0,0 +1,10 @@
+import sys
+sys.dont_write_bytecode = True
+
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import warnings
+
+warnings.filterwarnings('ignore')
