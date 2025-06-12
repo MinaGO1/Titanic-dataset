bb0fc474b9918dc96eed776dfa851f9f810432e6
Mina <salipmina123@gmail.com>
2025-06-12 21:30:15 +0300

    Made logistic regression and edited on data analysis notebooks, you should now save the models data

--- notebooks/auto_imports.py
+++ notebooks/auto_imports.py
@@ -7,4 +7,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import warnings
 
+from sklearn.metrics import accuracy_score , classification_report , confusion_matrix
+from sklearn.preprocessing import RobustScaler , LabelEncoder
+from sklearn.model_selection import train_test_split
+
 warnings.filterwarnings('ignore')
